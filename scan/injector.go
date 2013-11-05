5f83887bf50ea018fa162d27719a62b1b4b36634
Rafael Dantas Justo <rafael@registro.br>
2013-11-05 09:13:52 -0200

    Injecting value dependencies
    See #3

--- scan/injector.go
+++ scan/injector.go
@@ -16,17 +16,13 @@ type Injector struct {
 	Database *mgo.Database
 }
 
-// Method that starts the injector job, retrieving the data and adding the same data into
-// a channel
-func (i Injector) Start(queue chan model.Domain) error {
-	// Strategy:
-	//
-	// For domains that have all DNS OK and the DS OK, we check at least once a week (7
-	// days), for the other cases we check at least once every 2 days. The longer the last
-	// check occurred, better are the chances to select the domain for the scan.
-	//
-	// TODO: Dates must be configurable
-
+// Method that starts the injector job, retrieving the data from the database and adding
+// the same data into a channel for a querier start sending DNS requests. There's also
+// three more parameters to define the maximum number of days to verify a domain
+// configured correctly with DNS/DNSSEC, a domain with problems and to alert for DNSSEC
+// signatures that are near from the expiration date
+func (i Injector) Start(domainsToQueryChannel chan model.Domain,
+	maxOKVerificationDays, maxErrorVerificationDays, maxExpirationAlertDays int) error {
 	var domainDAO dao.DomainDAO
 
 	// Load all domains from database to begin the scan
@@ -36,8 +32,8 @@ func (i Injector) Start(queue chan model.Domain) error {
 	}
 
 	// Start the random with seed only once, we are going to reuse it on every domain
-	// check to randomly a domain to the scan or not. As we are using the current
-	// nanosecond we have the entropy necessary to be really random
+	// check to randomly select a domain to the scan or not. As we are using the current
+	// nanosecond, we have the entropy necessary to be really random
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	for {
@@ -49,22 +45,25 @@ func (i Injector) Start(queue chan model.Domain) error {
 		// When all nameservers are OK the domain has less chance to be selected for the
 		// scan, because the random range will be bigger
 		if domain.AllNameserversOK() && domain.AllDSSetOK() {
-			maxDays = 7
+			maxDays = maxOKVerificationDays
 		} else {
-			maxDays = 2
+			maxDays = maxErrorVerificationDays
 		}
 
+		// The longer the last check occurred, better are the chances to select the domain
+		// for the scan
 		daysSinceLastCheck := domain.DaysSinceLastCheck()
 		selectedDay := 1 + (random.Int() * maxDays / math.MaxInt64)
 
 		// If the domain is configured with DNSSEC and is near the expiration date, we
 		// must check even if it's not selected by the random algorithm
-		if !domain.IsNearDNSSECExpirationDate(10) && selectedDay > daysSinceLastCheck {
+		if !domain.IsNearDNSSECExpirationDate(maxExpirationAlertDays) &&
+			selectedDay > daysSinceLastCheck {
 			continue
 		}
 
 		// Send to the querier
-		queue <- domain
+		domainsToQueryChannel <- domain
 	}
 
 	return nil
