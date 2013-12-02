5bad47cf3a56881a14693cd6421e3f0c4cab49de
Rafael Dantas Justo <rafael@registro.br>
2013-12-02 09:40:30 -0200

    Using cover parameter in tests from Go 1.2

--- test-all.sh
+++ test-all.sh
@@ -6,7 +6,7 @@ current_dir=$PWD
 # Unit tests
 echo "\n[[ UNIT TESTS ]]\n"
 
-cd $GOPATH/src/shelter && find . -type f -name '*_test.go' -exec dirname {} \; | sort -u | awk '{printf("shelter%s ", substr($1,2))}' | xargs -I@ sh -c "go install @ && go test @"
+cd $GOPATH/src/shelter && find . -type f -name '*_test.go' -exec dirname {} \; | sort -u | awk '{printf("shelter%s ", substr($1,2))}' | xargs -I@ sh -c "go install @ && go test -cover @"
 return_code=$?
 
 # If there was any error in the unit tests, we shouldn't run
