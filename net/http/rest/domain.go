0cd05386c9a9c3dbea5ce35a4a2d71d6694ea69b
Rafael Dantas Justo <rafael@registro.br>
2013-12-11 16:29:12 -0200

    More comments for design brain storm

--- net/http/rest/domain.go
+++ net/http/rest/domain.go
@@ -35,4 +35,40 @@ func handleDomain(w http.ResponseWriter, r *http.Request) {
 	//   Status
 	//   Accept-Language
 	//   Accept
+
+	// TODO Check:
+	//   Accept
+	//   Accept-Charset
+	//   Accept-Language
+	//   Content-MD5
+	//   Content-Type
+	//   Authorization
+
+	if r.Method == "GET" {
+		retrieveDomain(w, r)
+
+	} else if r.Method == "PUT" {
+		createUpdateDomain(w, r)
+
+	} else if r.Method == "DELETE" {
+		removeDomain(w, r)
+
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func retrieveDomain(w http.ResponseWriter, r *http.Request) {
+	// TODO Check:
+	//   If-Modified-Since
+	//   If-Match
+	//   If-None-Match
+}
+
+func createUpdateDomain(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func removeDomain(w http.ResponseWriter, r *http.Request) {
+
 }
