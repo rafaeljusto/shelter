35d6057c86dd1d7d5646e5c1c406a1fcc1875754
Rafael Dantas Justo <rafael@registro.br>
2014-01-13 08:47:24 -0200

    Finish testing REST mux

--- testing/rest_mux/rest_mux.go
+++ testing/rest_mux/rest_mux.go
@@ -13,6 +13,7 @@ import (
 	"shelter/database/mongodb"
 	"shelter/net/http/rest"
 	"shelter/net/http/rest/check"
+	"shelter/net/http/rest/messages"
 	"shelter/testing/utils"
 	"time"
 )
@@ -61,13 +62,14 @@ func main() {
 	domainDAO.RemoveAll()
 
 	var mux rest.Mux
+	checkHeaders(&mux)
 	createDomain(&mux)
 	deleteDomain(&mux)
 
 	utils.Println("SUCCESS!")
 }
 
-func createDomain(mux *rest.Mux) {
+func checkHeaders(mux *rest.Mux) {
 	r, err := http.NewRequest("GET", "/xxx/example.com.br.", nil)
 	if err != nil {
 		utils.Fatalln("Error creting the HTTP request", err)
@@ -80,6 +82,73 @@ func createDomain(mux *rest.Mux) {
 		utils.Fatalln("Not returning HTTP Service Unavailable when the URI is not registered", nil)
 	}
 
+	data := []struct {
+		Header             string
+		HeaderValue        string
+		ExpectedHTTPStatus int
+	}{
+		{
+			Header:             "Accept-Language",
+			HeaderValue:        "xxx",
+			ExpectedHTTPStatus: http.StatusNotAcceptable,
+		},
+		{
+			Header:             "Accept",
+			HeaderValue:        "xxx",
+			ExpectedHTTPStatus: http.StatusNotAcceptable,
+		},
+		{
+			Header:             "Accept-Charset",
+			HeaderValue:        "xxx",
+			ExpectedHTTPStatus: http.StatusNotAcceptable,
+		},
+		{
+			Header:             "Content-Type",
+			HeaderValue:        "xxx",
+			ExpectedHTTPStatus: http.StatusBadRequest,
+		},
+		{
+			Header:             "Content-Type",
+			HeaderValue:        "",
+			ExpectedHTTPStatus: http.StatusBadRequest,
+		},
+		{
+			Header:             "Content-MD5",
+			HeaderValue:        "xxx",
+			ExpectedHTTPStatus: http.StatusBadRequest,
+		},
+		{
+			Header:             "Content-MD5",
+			HeaderValue:        "",
+			ExpectedHTTPStatus: http.StatusBadRequest,
+		},
+		{
+			Header:             "Date",
+			HeaderValue:        "xxx",
+			ExpectedHTTPStatus: http.StatusBadRequest,
+		},
+		{
+			Header:             "Date",
+			HeaderValue:        "",
+			ExpectedHTTPStatus: http.StatusBadRequest,
+		},
+		{
+			Header:             "Authorization",
+			HeaderValue:        "xxx",
+			ExpectedHTTPStatus: http.StatusBadRequest,
+		},
+		{
+			Header:             "Authorization",
+			HeaderValue:        fmt.Sprintf("%s %d:%s", check.SupportedNamespace, 999, "0PN5J17HBGZHT7JJ3X82"),
+			ExpectedHTTPStatus: http.StatusBadRequest,
+		},
+		{
+			Header:             "Authorization",
+			HeaderValue:        fmt.Sprintf("%s %d:%s", check.SupportedNamespace, 1, "0PN5J17HBGZHT7JJ3X82"),
+			ExpectedHTTPStatus: http.StatusUnauthorized,
+		},
+	}
+
 	content := []byte(`{
       "Nameservers": [
         { "Host": "ns1.example.com.br.", "ipv4": "127.0.0.1" },
@@ -90,7 +159,39 @@ func createDomain(mux *rest.Mux) {
       ]
     }`)
 
-	r, err = http.NewRequest("PUT", "/domain/example.com.br.",
+	for _, item := range data {
+		r, err = http.NewRequest("PUT", "/domain/example.com.br.",
+			bytes.NewReader(content))
+		if err != nil {
+			utils.Fatalln("Error creting the HTTP request", err)
+		}
+
+		buildHTTPHeader(r, content)
+		r.Header.Set(item.Header, item.HeaderValue)
+
+		w = httptest.NewRecorder()
+		mux.ServeHTTP(w, r)
+
+		if w.Code != item.ExpectedHTTPStatus {
+			utils.Fatalln(fmt.Sprintf("For HTTP header %s with the value '%s' we were "+
+				"expecting the HTTP status %d but got %d",
+				item.Header, item.HeaderValue, item.ExpectedHTTPStatus, w.Code), nil)
+		}
+	}
+}
+
+func createDomain(mux *rest.Mux) {
+	content := []byte(`{
+      "Nameservers": [
+        { "Host": "ns1.example.com.br.", "ipv4": "127.0.0.1" },
+        { "Host": "ns2.example.com.br.", "ipv6": "::1" }
+      ],
+      "Owners": [
+        "admin@example.com.br."
+      ]
+    }`)
+
+	r, err := http.NewRequest("PUT", "/domain/example.com.br.",
 		bytes.NewReader(content))
 	if err != nil {
 		utils.Fatalln("Error creting the HTTP request", err)
@@ -98,12 +199,42 @@ func createDomain(mux *rest.Mux) {
 
 	buildHTTPHeader(r, content)
 
-	w = httptest.NewRecorder()
+	messages.ShelterRESTLanguagePacks = messages.LanguagePacks{
+		Default: "en-us",
+		Packs: []messages.LanguagePack{
+			{
+				GenericName:  "en",
+				SpecificName: "en-us",
+			},
+			{
+				GenericName:  "pt",
+				SpecificName: "pt-br",
+			},
+		},
+	}
+
+	w := httptest.NewRecorder()
 	mux.ServeHTTP(w, r)
 
 	if w.Code != http.StatusCreated {
 		utils.Fatalln(fmt.Sprintf("Error creting a domain object. Expected HTTP status %d and got %d",
 			http.StatusCreated, w.Code), nil)
+	}
+
+	if len(w.Header().Get("Accept")) == 0 {
+		utils.Fatalln("Not setting Accept HTTP header in response", nil)
+	}
+
+	if len(w.Header().Get("Accept-Language")) == 0 {
+		utils.Fatalln("Not setting Accept-Language HTTP header in response", nil)
+	}
+
+	if len(w.Header().Get("Accept-Charset")) == 0 {
+		utils.Fatalln("Not setting Accept-Charset HTTP header in response", nil)
+	}
+
+	if len(w.Header().Get("Date")) == 0 {
+		utils.Fatalln("Not setting Date HTTP header in response", nil)
 	}
 }
 
