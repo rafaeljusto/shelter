cb65cfcb3ada8b17f25d1a4f0b4fc8dc2df37445
Rafael Dantas Justo <rafael@registro.br>
2014-01-21 16:01:01 -0200

    Fix test scripts for new project path

--- test-all.sh
+++ test-all.sh
@@ -3,15 +3,10 @@
 : ${GOPATH:?"Need to set GOPATH"}
 option=$1
 
-path="$GOPATH/src/github.com/rafaeljusto/shelter/"
-if [ "$option" = "-github" ]; then
-  path="./"
-fi
-
 # Main binary must build
-cd $path && go build shelter.go
+cd $GOPATH/src/github.com/rafaeljusto/shelter && go build shelter.go
 return_code=$?
-cd $path && rm -f shelter
+cd $GOPATH/src/github.com/rafaeljusto/shelter && rm -f shelter
 
 # If there was any error building the main binary, we shouldn't continue
 if [ $return_code -ne 0 ]; then
@@ -21,7 +16,7 @@ fi
 # Unit tests
 echo "\n[[ UNIT TESTS ]]\n"
 
-cd $path && find . -type f -name '*_test.go' -exec dirname {} \; | sort -u | awk '{printf("shelter%s ", substr($1,2))}' | xargs -I@ sh -c "go install @ && go test -cover @"
+cd $GOPATH/src/github.com/rafaeljusto/shelter && find . -type f -name '*_test.go' -exec dirname {} \; | sort -u | awk '{printf("github.com/rafaeljusto/shelter%s ", substr($1,2))}' | xargs -I@ sh -c "go install @ && go test -cover @"
 return_code=$?
 
 # If there was any error in the unit tests, we shouldn't run
@@ -38,7 +33,7 @@ fi
 # Integration tests
 echo "\n[[ INTEGRATION TESTS ]]\n"
 
-cd $path && find . -type f -wholename './testing/*.go' | grep -v 'utils' | sort -u | awk -F '/' '{ print $0 " -config=\"" substr($0, 1, length($0)-3) ".conf\"" }' | xargs -I@ sh -c "go run -race @"
+cd $GOPATH/src/github.com/rafaeljusto/shelter && find . -type f -wholename './testing/*.go' | grep -v 'utils' | sort -u | awk -F '/' '{ print $0 " -config=\"" substr($0, 1, length($0)-3) ".conf\"" }' | xargs -I@ sh -c "go run -race @"
 return_code=$?
 rm -f scan.log
 
--- test-cover-report.sh
+++ test-cover-report.sh
@@ -2,5 +2,5 @@
 
 : ${GOPATH:?"Need to set GOPATH"}
 
-cd $GOPATH/src/shelter && find . -type f -name '*_test.go' -exec dirname {} \; | sort -u | awk '{ print "shelter" substr($1,2) }' | xargs -I@ sh -c "go install @ && go test -coverprofile='cover-profile.out' -cover @ && go tool cover -html=cover-profile.out"
+cd $GOPATH/src/github.com/rafaeljusto/shelter/ && find . -type f -name '*_test.go' -exec dirname {} \; | sort -u | awk '{ print "github.com/rafaeljusto/shelter" substr($1,2) }' | xargs -I@ sh -c "go install @ && go test -coverprofile='cover-profile.out' -cover @ && go tool cover -html=cover-profile.out"
 rm -f cover-profile.out
