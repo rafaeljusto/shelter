3183c979a0f5f41f38b61e271579d905ef863f6d
Rafael Justo <rafael@justo.net.br>
2014-02-02 19:29:29 -0200

    Begin creating the Scan object

---
+++ dao/scan_dao.go
@@ -0,0 +1,59 @@
+package dao
+
+import (
+	"errors"
+	"github.com/rafaeljusto/shelter/model"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"time"
+)
+
+// List of possible errors that can occur in this DAO. There can be also other errors from
+// low level drivers.
+var (
+	// Programmer must set the Database attribute from DomainDAO with a valid connection
+	// before using this object
+	ErrScanDAOUndefinedDatabase = errors.New("No database defined for ScanDAO")
+)
+
+const (
+	scanDAOCollection = "scan" // Collection used to store all scan objects in the MongoDB database
+)
+
+// ScanDAO is the structure responsible for keeping the database connection to save a new scan
+// object after every execution
+type ScanDAO struct {
+	Database *mgo.Database // MongoDB Database
+}
+
+// Save the scan object in the database. On creation the scan object is going to receive the id that
+// refers to the entry in the database
+func (dao ScanDAO) Save(scan *model.Scan) error {
+	// Check if the programmer forgot to set the database in DomainDAO object
+	if dao.Database == nil {
+		return ErrScanDAOUndefinedDatabase
+	}
+
+	// When creating a new domain object, the id will be probably nil (or kind of new
+	// according to bson.ObjectId), so we must initialize it
+	if len(scan.Id.Hex()) == 0 {
+		scan.Id = bson.NewObjectId()
+	}
+
+	// Every time we modified a domain object we increase the revision counter to identify
+	// changes in high level structures
+	scan.Revision += 1
+
+	// Store the last time that the object was modified
+	scan.LastModifiedAt = time.Now().UTC()
+
+	// Upsert try to update the collection entry if exists, if not, it creates a new entry. For all
+	// the scan objects we are going to use the collection "scan". We also avoid concurency adding the
+	// revision as a paremeter for updating the entry
+	_, err := dao.Database.C(scanDAOCollection).Upsert(bson.M{
+		"_id":      scan.Id,
+		"revision": scan.Revision - 1,
+	}, scan)
+
+	return err
+}
---
+++ model/scan.go
@@ -0,0 +1,43 @@
+package model
+
+import (
+	"labix.org/v2/mgo/bson"
+	"time"
+)
+
+// List of possible values of a scan status. The status ScanStatusWaitingExecution,
+// ScanStatusLoadingData, ScanStatusRunning will only be visible in a CurrentScan struct
+const (
+	ScanStatusWaitingExecution ScanStatus = 0 // The scan is going to be executed in the future
+	ScanStatusLoadingData      ScanStatus = 1 // Loading the domains objects for Scan
+	ScanStatusRunning          ScanStatus = 2 // The scan is current scanning the system
+	ScanStatusExecuted         ScanStatus = 3 // Scan alredy finished succesfully
+	ScanStatusAborted          ScanStatus = 4 // Scan had problems during the execution
+)
+
+// We keep a state from the scan to identify scans that had problem or not, and the current
+// executio. This is useful for reports and a richer interface
+type ScanStatus int
+
+// Store all data related to a scan executed on the system
+type Scan struct {
+	Id                      bson.ObjectId            `bson:"_id"` // Database identification
+	Revision                int                      // Version of the object
+	Status                  ScanStatus               // Status of the scan
+	StartedAt               time.Time                // Date and time that the scan started
+	FinishedAt              time.Time                // Date and time that the scan finished
+	LastModifiedAt          time.Time                // Last time the object was modified
+	DomainsScanned          int                      // Number of domains scanned
+	DomainsWihDNSSECScanned int                      // Number of domains with DS recods scanned
+	NameserverStatistics    map[NameserverStatus]int // Statistics from nameserver status
+	DSStatistics            map[DSStatus]int         // Statistics from DS records' status
+}
+
+// CurrentScan is a Scan that is the next to be executed or is executing at this moment. The data
+// from this struct is not stored until the scan is finished and become only a Scan struct. This
+// should be used to tell the user (using a service) how is a progress of a scan on-the-fly
+type CurrentScan struct {
+	Scan                   // CurrentScan is a Scan
+	DomainsToBeScanned int // Domains selected to be scanned
+	Progress           int // Current progress, in percentage, of the scan execution
+}
