311c3e3af0c5a89c44eca64dc3f3b225e9fe1efc
Rafael Justo <rafael@justo.net.br>
2014-02-07 23:40:57 -0200

    Remove duplicated code

---
+++ net/http/rest/handler/cache.go
@@ -0,0 +1,132 @@
+package handler
+
+import (
+	"github.com/rafaeljusto/shelter/log"
+	"github.com/rafaeljusto/shelter/net/http/rest/check"
+	"github.com/rafaeljusto/shelter/net/http/rest/context"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func CheckHTTPCacheHeaders(
+	r *http.Request,
+	context *context.Context,
+	lastModified time.Time,
+	revision int,
+) bool {
+
+	return checkIfModifiedSince(r, context, lastModified) &&
+		checkIfUnmodifiedSince(r, context, lastModified) &&
+		checkIfMatch(r, context, revision) &&
+		checkIfNoneMatch(r, context, revision)
+}
+
+func checkIfModifiedSince(r *http.Request, context *context.Context, lastModified time.Time) bool {
+	modifiedSince, err := check.HTTPIfModifiedSince(r, lastModified)
+	if err != nil {
+		if err := context.MessageResponse(http.StatusBadRequest,
+			"invalid-header-date", r.URL.RequestURI()); err != nil {
+
+			log.Println("Error while writing response. Details:", err)
+			context.Response(http.StatusInternalServerError)
+		}
+		return false
+
+	} else if !modifiedSince {
+		// If the requested variant has not been modified since the time specified in this
+		// field, an entity will not be returned from the server; instead, a 304 (not
+		// modified) response will be returned without any message-body
+		context.Response(http.StatusNotModified)
+		return false
+	}
+
+	return true
+}
+
+func checkIfUnmodifiedSince(r *http.Request, context *context.Context, lastModified time.Time) bool {
+	unmodifiedSince, err := check.HTTPIfUnmodifiedSince(r, lastModified)
+	if err != nil {
+		if err := context.MessageResponse(http.StatusBadRequest,
+			"invalid-header-date", r.URL.RequestURI()); err != nil {
+
+			log.Println("Error while writing response. Details:", err)
+			context.Response(http.StatusInternalServerError)
+		}
+		return false
+
+	} else if !unmodifiedSince {
+		// If the requested variant has been modified since the specified time, the server
+		// MUST NOT perform the requested operation, and MUST return a 412 (Precondition
+		// Failed)
+		context.Response(http.StatusPreconditionFailed)
+		return false
+	}
+
+	return true
+}
+
+func checkIfMatch(r *http.Request, context *context.Context, revision int) bool {
+	match, err := check.HTTPIfMatch(r, revision)
+	if err != nil {
+		if err := context.MessageResponse(http.StatusBadRequest,
+			"invalid-if-match", r.URL.RequestURI()); err != nil {
+
+			log.Println("Error while writing response. Details:", err)
+			context.Response(http.StatusInternalServerError)
+		}
+		return false
+
+	} else if !match {
+		// If "*" is given and no current entity exists or if none of the entity tags match
+		// the server MUST NOT perform the requested method, and MUST return a 412
+		// (Precondition Failed) response
+		if err := context.MessageResponse(http.StatusPreconditionFailed,
+			"if-match-failed", r.URL.RequestURI()); err != nil {
+
+			log.Println("Error while writing response. Details:", err)
+			context.Response(http.StatusInternalServerError)
+		}
+		return false
+	}
+
+	return true
+}
+
+func checkIfNoneMatch(r *http.Request, context *context.Context, revision int) bool {
+	noneMatch, err := check.HTTPIfNoneMatch(r, revision)
+	if err != nil {
+		if err := context.MessageResponse(http.StatusBadRequest,
+			"invalid-if-none-match", r.URL.RequestURI()); err != nil {
+
+			log.Println("Error while writing response. Details:", err)
+			context.Response(http.StatusInternalServerError)
+		}
+		return false
+
+	} else if !noneMatch {
+		// Instead, if the request method was GET or HEAD, the server SHOULD respond with a
+		// 304 (Not Modified) response, including the cache-related header fields
+		// (particularly ETag) of one of the entities that matched. For all other request
+		// methods, the server MUST respond with a status of 412 (Precondition Failed)
+		if r.Method == "GET" || r.Method == "HEAD" {
+			context.AddHeader("ETag", strconv.Itoa(revision))
+			if err := context.MessageResponse(http.StatusNotModified,
+				"if-match-none-failed", r.URL.RequestURI()); err != nil {
+
+				log.Println("Error while writing response. Details:", err)
+				context.Response(http.StatusInternalServerError)
+			}
+
+		} else {
+			if err := context.MessageResponse(http.StatusPreconditionFailed,
+				"if-match-none-failed", r.URL.RequestURI()); err != nil {
+
+				log.Println("Error while writing response. Details:", err)
+				context.Response(http.StatusInternalServerError)
+			}
+		}
+		return false
+	}
+	return true
+}
--- net/http/rest/handler/domain.go
+++ net/http/rest/handler/domain.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"github.com/rafaeljusto/shelter/dao"
 	"github.com/rafaeljusto/shelter/log"
-	"github.com/rafaeljusto/shelter/net/http/rest/check"
 	"github.com/rafaeljusto/shelter/net/http/rest/context"
 	"github.com/rafaeljusto/shelter/net/http/rest/protocol"
 	"net/http"
@@ -57,87 +56,7 @@ func retrieveDomain(r *http.Request, context *context.Context, fqdn string) {
 		return
 	}
 
-	modifiedSince, err := check.HTTPIfModifiedSince(r, domain.LastModifiedAt)
-	if err != nil {
-		if err := context.MessageResponse(http.StatusBadRequest,
-			"invalid-header-date", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-
-	} else if !modifiedSince {
-		// If the requested variant has not been modified since the time specified in this
-		// field, an entity will not be returned from the server; instead, a 304 (not
-		// modified) response will be returned without any message-body
-		context.Response(http.StatusNotModified)
-		return
-	}
-
-	unmodifiedSince, err := check.HTTPIfUnmodifiedSince(r, domain.LastModifiedAt)
-	if err != nil {
-		if err := context.MessageResponse(http.StatusBadRequest,
-			"invalid-header-date", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-
-	} else if !unmodifiedSince {
-		// If the requested variant has been modified since the specified time, the server
-		// MUST NOT perform the requested operation, and MUST return a 412 (Precondition
-		// Failed)
-		context.Response(http.StatusPreconditionFailed)
-		return
-	}
-
-	match, err := check.HTTPIfMatch(r, domain.Revision)
-	if err != nil {
-		if err := context.MessageResponse(http.StatusBadRequest,
-			"invalid-if-match", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-
-	} else if !match {
-		// If "*" is given and no current entity exists or if none of the entity tags match
-		// the server MUST NOT perform the requested method, and MUST return a 412
-		// (Precondition Failed) response
-		if err := context.MessageResponse(http.StatusPreconditionFailed,
-			"if-match-failed", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-	}
-
-	noneMatch, err := check.HTTPIfNoneMatch(r, domain.Revision)
-	if err != nil {
-		if err := context.MessageResponse(http.StatusBadRequest,
-			"invalid-if-none-match", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-
-	} else if !noneMatch {
-		// Instead, if the request method was GET or HEAD, the server SHOULD respond with a
-		// 304 (Not Modified) response, including the cache-related header fields
-		// (particularly ETag) of one of the entities that matched. For all other request
-		// methods, the server MUST respond with a status of 412 (Precondition Failed)
-		context.AddHeader("ETag", strconv.Itoa(domain.Revision))
-		if err := context.MessageResponse(http.StatusNotModified,
-			"if-match-none-failed", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
+	if !CheckHTTPCacheHeaders(r, context, domain.LastModifiedAt, domain.Revision) {
 		return
 	}
 
@@ -178,89 +97,11 @@ func createUpdateDomain(r *http.Request, context *context.Context, fqdn string) 
 	// user, if the domain does not exist yet thats alright because we will create it
 	domain, _ := domainDAO.FindByFQDN(fqdn)
 
-	modifiedSince, err := check.HTTPIfModifiedSince(r, domain.LastModifiedAt)
-	if err != nil {
-		if err := context.MessageResponse(http.StatusBadRequest,
-			"invalid-header-date", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-
-	} else if !modifiedSince {
-		// If the requested variant has not been modified since the time specified in this
-		// field, an entity will not be returned from the server; instead, a 304 (not
-		// modified) response will be returned without any message-body
-		context.Response(http.StatusNotModified)
+	if !CheckHTTPCacheHeaders(r, context, domain.LastModifiedAt, domain.Revision) {
 		return
 	}
 
-	unmodifiedSince, err := check.HTTPIfUnmodifiedSince(r, domain.LastModifiedAt)
-	if err != nil {
-		if err := context.MessageResponse(http.StatusBadRequest,
-			"invalid-header-date", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-
-	} else if !unmodifiedSince {
-		// If the requested variant has been modified since the specified time, the server
-		// MUST NOT perform the requested operation, and MUST return a 412 (Precondition
-		// Failed)
-		context.Response(http.StatusPreconditionFailed)
-		return
-	}
-
-	match, err := check.HTTPIfMatch(r, domain.Revision)
-	if err != nil {
-		if err := context.MessageResponse(http.StatusBadRequest,
-			"invalid-if-match", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-
-	} else if !match {
-		// If "*" is given and no current entity exists or if none of the entity tags match
-		// the server MUST NOT perform the requested method, and MUST return a 412
-		// (Precondition Failed) response
-		if err := context.MessageResponse(http.StatusPreconditionFailed,
-			"if-match-failed", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-	}
-
-	noneMatch, err := check.HTTPIfNoneMatch(r, domain.Revision)
-	if err != nil {
-		if err := context.MessageResponse(http.StatusBadRequest,
-			"invalid-if-none-match", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-
-	} else if !noneMatch {
-		// Instead, if the request method was GET or HEAD, the server SHOULD respond with a
-		// 304 (Not Modified) response, including the cache-related header fields
-		// (particularly ETag) of one of the entities that matched. For all other request
-		// methods, the server MUST respond with a status of 412 (Precondition Failed)
-		if err := context.MessageResponse(http.StatusPreconditionFailed,
-			"if-match-none-failed", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-	}
-
+	var err error
 	if domain, err = protocol.Merge(domain, domainRequest); err != nil {
 		log.Println("Error while merging domain objects for create or "+
 			"update operation. Details:", err)
@@ -308,86 +149,7 @@ func removeDomain(r *http.Request, context *context.Context, fqdn string) {
 		return
 	}
 
-	modifiedSince, err := check.HTTPIfModifiedSince(r, domain.LastModifiedAt)
-	if err != nil {
-		if err := context.MessageResponse(http.StatusBadRequest,
-			"invalid-header-date", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-
-	} else if !modifiedSince {
-		// If the requested variant has not been modified since the time specified in this
-		// field, an entity will not be returned from the server; instead, a 304 (not
-		// modified) response will be returned without any message-body
-		context.Response(http.StatusNotModified)
-		return
-	}
-
-	unmodifiedSince, err := check.HTTPIfUnmodifiedSince(r, domain.LastModifiedAt)
-	if err != nil {
-		if err := context.MessageResponse(http.StatusBadRequest,
-			"invalid-header-date", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-
-	} else if !unmodifiedSince {
-		// If the requested variant has been modified since the specified time, the server
-		// MUST NOT perform the requested operation, and MUST return a 412 (Precondition
-		// Failed)
-		context.Response(http.StatusPreconditionFailed)
-		return
-	}
-
-	match, err := check.HTTPIfMatch(r, domain.Revision)
-	if err != nil {
-		if err := context.MessageResponse(http.StatusBadRequest,
-			"invalid-if-match", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-
-	} else if !match {
-		// If "*" is given and no current entity exists or if none of the entity tags match
-		// the server MUST NOT perform the requested method, and MUST return a 412
-		// (Precondition Failed) response
-		if err := context.MessageResponse(http.StatusPreconditionFailed,
-			"if-match-failed", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-	}
-
-	noneMatch, err := check.HTTPIfNoneMatch(r, domain.Revision)
-	if err != nil {
-		if err := context.MessageResponse(http.StatusBadRequest,
-			"invalid-if-none-match", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
-		return
-
-	} else if !noneMatch {
-		// Instead, if the request method was GET or HEAD, the server SHOULD respond with a
-		// 304 (Not Modified) response, including the cache-related header fields
-		// (particularly ETag) of one of the entities that matched. For all other request
-		// methods, the server MUST respond with a status of 412 (Precondition Failed)
-		if err := context.MessageResponse(http.StatusPreconditionFailed,
-			"if-match-none-failed", r.URL.RequestURI()); err != nil {
-
-			log.Println("Error while writing response. Details:", err)
-			context.Response(http.StatusInternalServerError)
-		}
+	if !CheckHTTPCacheHeaders(r, context, domain.LastModifiedAt, domain.Revision) {
 		return
 	}
 
