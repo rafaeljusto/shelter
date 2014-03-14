b4148268ffa6f177bd96c51641da41413bc21d3b
Rafael Dantas Justo <rafael@registro.br>
2014-03-14 13:11:09 -0300

    Moving domain form to an AngularJS directive

--- templates/client/js/shelter.js
+++ templates/client/js/shelter.js
@@ -1,6 +1,78 @@
 // References:
 // http://stackoverflow.com/questions/17646034/what-is-the-best-practice-for-making-an-ajax-call-in-angular-js
 
+var dnskeyFlags = [
+  {id: 256, name:"ZSK"},
+  {id: 257, name:"KSK"},
+];
+
+var algorithms = [
+  {id: 1, name:"RSA/MD5"},
+  {id: 2, name:"DH"},
+  {id: 3, name:"DSA/SHA1"},
+  {id: 4, name:"ECC"},
+  {id: 5, name:"RSA/SHA1"},
+  {id: 6, name:"DSA/SHA1-NSEC3"},
+  {id: 7, name:"RSA/SHA1-NSEC3"},
+  {id: 8, name:"RSA/SHA256"},
+  {id: 10, name:"RSA/SHA512"},
+  {id: 12, name:"GOST R"},
+  {id: 13, name:"ECDSA/SHA256"},
+  {id: 14, name:"ECDSA/SHA384"}
+];
+
+var dsDigestTypes = [
+  {id: 1, name: "SHA1"},
+  {id: 2, name: "SHA256"},
+  {id: 3, name: "GOST94"},
+  {id: 4, name: "SHA384"},
+  {id: 5, name: "SHA512"},
+];
+
+var ownerLanguages = [
+  "en-US",
+  "pt-BR"
+];
+
+var emptyNameserver = {
+  host: "",
+  ipv4: "",
+  ipv6: ""
+};
+
+var emptyDNSKEY = {
+  flags: 257,
+  algorithm: 8,
+  publickKey: ""
+};
+
+var emptyDS = {
+  keytag: "",
+  algorithm: 8,
+  digestType: 2,
+  digest: ""
+};
+
+var emptyOwner = {
+  email: "",
+  language: "en-US"
+};
+
+var emptyDomain = {
+  fqdn: "",
+  nameservers: [
+    angular.copy(emptyNameserver),
+    angular.copy(emptyNameserver)
+  ],
+  dnskeys: [],
+  dsset: [
+    angular.copy(emptyDS)
+  ],
+  owners: [
+    angular.copy(emptyOwner)
+  ]
+};
+
 angular.module("shelter", ["ngCookies", "pascalprecht.translate"])
 
   .config(function($translateProvider, $httpProvider) {
@@ -36,8 +108,13 @@ angular.module("shelter", ["ngCookies", "pascalprecht.translate"])
 
   .filter("datetime", function() {
     return function(input, language) {
-      var datetime = moment("YYYY-MM-DDTHH:mm:ssZ", input)
+      var datetime = moment(input, ["YYYY-MM-DDTHH:mm:ss.SSSZ", "YYYY-MM-DDTHH:mm:ssZ"])
       if (datetime.isValid()) {
+        // Detect empty datetime
+        if (datetime.unix() <= 0) {
+          return ""
+        }
+
         language = language.replace("_", "-");
         moment.lang(language);
         return datetime.format("MMMM Do YYYY, h:mm:ss a");
@@ -125,132 +202,91 @@ angular.module("shelter", ["ngCookies", "pascalprecht.translate"])
         $scope.getLanguage = function() {
           return $translate.use();
         };
+      }
+    };
+  })
+
+  .directive("domainform", function() {
+    return {
+      restrict: 'E',
+      scope: {
+        domain: '='
+      },
+      templateUrl: "/directives/domainform.html",
+      controller: function($scope, $translate, domainService) {
+        if ($scope.domain == emptyDomain) {
+          $scope.domain = angular.copy($scope.domain);
+        }
+
+        $scope.emptyNameserver = angular.copy(emptyNameserver);
+        $scope.emptyDNSKEY = angular.copy(emptyDNSKEY);
+        $scope.emptyDS = angular.copy(emptyDS);
+        $scope.emptyOwner = angular.copy(emptyOwner);
+
+        // Don't need to copy constant values
+        $scope.dnskeyFlags = dnskeyFlags;
+        $scope.algorithms = algorithms;
+        $scope.dsDigestTypes = dsDigestTypes;
+        $scope.ownerLanguages = ownerLanguages;
+
+        $scope.addToList = function(object, list) {
+          list.push(angular.copy(object));
+        };
+
+        $scope.removeFromList = function(index, list) {
+          if (index >= 0 && index < list.length) {
+            list.splice(index, 1);
+          }
+        };
 
         $scope.saveDomain = function(domain) {
-          domainService.saveDomain(domain).then(
+          // Convert keytag to number. For now I don't want to use valueAsNumber function
+          // because the code will remain the same size and older browsers will break
+          for (var i = 0; i < $scope.domain.dsset.length; i += 1) {
+            var keytag = parseInt($scope.domain.dsset[i].keytag, 10);
+            if (keytag == NaN) {
+              keytag = 0;
+            }
+            $scope.domain.dsset[i].keytag = keytag;
+          }
+
+          domainService.saveDomain($scope.domain).then(
             function(response) {
-              // TODO
+              if (response.status == 201 && response.status == 204) {
+                $translate("Domain created").then(function(translation) {
+                  $scope.success = translation;
+                });
+              } else if (response.status == 400) {
+                $scope.error = response.data.message;
+              } else {
+                $translate("Server error").then(function(translation) {
+                  $scope.error = translation;
+                });
+              }
             });
         };
       }
     };
   })
 
-  .controller("domainCtrl", function($scope, $timeout, $translate, domainService) {
-    $scope.dnskeyFlags = [
-      {id: 256, name:"ZSK"},
-      {id: 257, name:"KSK"},
-    ];
-
-    $scope.algorithms = [
-      {id: 1, name:"RSA/MD5"},
-      {id: 2, name:"DH"},
-      {id: 3, name:"DSA/SHA1"},
-      {id: 4, name:"ECC"},
-      {id: 5, name:"RSA/SHA1"},
-      {id: 6, name:"DSA/SHA1-NSEC3"},
-      {id: 7, name:"RSA/SHA1-NSEC3"},
-      {id: 8, name:"RSA/SHA256"},
-      {id: 10, name:"RSA/SHA512"},
-      {id: 12, name:"GOST R"},
-      {id: 13, name:"ECDSA/SHA256"},
-      {id: 14, name:"ECDSA/SHA384"}
-    ];
-
-    $scope.dsDigestTypes = [
-      {id: 1, name: "SHA1"},
-      {id: 2, name: "SHA256"},
-      {id: 3, name: "GOST94"},
-      {id: 4, name: "SHA384"},
-      {id: 5, name: "SHA512"},
-    ];
-
-    $scope.ownerLanguages = [
-      "en-US",
-      "pt-BR"
-    ];
-
-    $scope.emptyNameserver = {
-      host: "",
-      ipv4: "",
-      ipv6: ""
-    };
-
-    $scope.emptyDNSKEY = {
-      flags: 257,
-      algorithm: 8,
-      publickKey: ""
-    };
-
-    $scope.emptyDS = {
-      keytag: "",
-      algorithm: 8,
-      digestType: 2,
-      digest: ""
-    };
-
-    $scope.emptyOwner = {
-      email: "",
-      language: "en-US"
-    };
-
-    $scope.domain = {
-      fqdn: "",
-      nameservers: [
-        angular.copy($scope.emptyNameserver),
-        angular.copy($scope.emptyNameserver)
-      ],
-      dnskeys: [],
-      dsset: [
-        angular.copy($scope.emptyDS)
-      ],
-      owners: [
-        angular.copy($scope.emptyOwner)
-      ]
-    };
-
-    $scope.addToList = function(object, list) {
-      list.push(angular.copy(object));
-    };
-
-    $scope.removeFromList = function(index, list) {
-      if (index >= 0 && index < list.length) {
-        list.splice(index, 1);
-      }
-    };
-
-    $scope.saveDomain = function() {
-      // Convert keytag to number. For now I don't want to use valueAsNumber function
-      // because the code will remain the same size and older browsers will break
-      for (var i = 0; i < $scope.domain.dsset.length; i += 1) {
-        var keytag = parseInt($scope.domain.dsset[i].keytag, 10);
-        if (keytag == NaN) {
-          keytag = 0;
-        }
-        $scope.domain.dsset[i].keytag = keytag;
-      }
-
-      domainService.saveDomain($scope.domain).then(
-        function(response) {
-          if (response.status == 400) {
-            $scope.error = response.data.message;
-          } else if (response.status != 201 && response.status != 204) {
-            $scope.error = $translate("Server error");
-          }
-        });
-    };
+  .controller("domainCtrl", function($scope) {
+    $scope.emptyDomain =  angular.copy(emptyDomain);
   })
 
-  .controller("domainsCtrl", function($scope, $timeout, domainService) {
+  .controller("domainsCtrl", function($scope, $translate, domainService) {
     $scope.pageSizes = [ 20, 40, 60, 80, 100 ];
 
     $scope.retrieveDomains = function(page, pageSize) {
       domainService.retrieveDomains(page, pageSize).then(
         function(response) {
-          if (response.status == 400) {
+          if (response.status == 200) {
+           $scope.pagination = response.data;
+          } else if (response.status == 400) {
             $scope.error = response.data.message;
           } else {
-           $scope.pagination = response.data;
+            $translate("Server error").then(function(translation) {
+              $scope.error = translation;
+            });
           }
         });
     };
@@ -328,7 +364,7 @@ angular.module("shelter", ["ngCookies", "pascalprecht.translate"])
     };
   })
 
-  .controller("scanCtrl", function($scope, $timeout, scanService) {
+  .controller("scanCtrl", function($scope, $translate, scanService) {
     $scope.pageSizes = [ 20, 40, 60, 80, 100 ];
 
     $scope.retrieveScans = function(page, pageSize) {
@@ -336,8 +372,12 @@ angular.module("shelter", ["ngCookies", "pascalprecht.translate"])
         function(response) {
           if (response.status == 200) {
             $scope.pagination = response.data;
+          } else if (response.status == 400) {
+            $scope.error = response.data.message;
           } else {
-            // TODO
+            $translate("Server error").then(function(translation) {
+              $scope.error = translation;
+            });
           }
         });
     };
