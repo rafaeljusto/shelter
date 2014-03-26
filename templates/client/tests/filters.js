735acc737577b2f18c8c8a6a231b204f10300d3e
Rafael Dantas Justo <rafael@registro.br>
2014-03-26 10:25:57 -0300

    Travis CI works with UTC timezone

--- templates/client/tests/filters.js
+++ templates/client/tests/filters.js
@@ -9,6 +9,7 @@ describe("Filters", function() {
   }));
 
   it("should format correctly a datetime", function() {
+    moment().zone("-03:00");
     expect(datetimeFilter("2014-03-26T09:55:29-03:00", "en_US")).toBe("March 26th 2014, 9:55:29 am");
     expect(datetimeFilter("2014-03-26T09:55:29-03:00", "pt_BR")).toBe("Março 26º 2014, 9:55:29 am");
   });
