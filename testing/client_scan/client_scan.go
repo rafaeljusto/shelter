b4ea74afc1c6a77a4d24fc80f055c7688788f048
Rafael Dantas Justo <rafael@registro.br>
2014-03-27 15:09:29 -0300

    Finish testing web client services

---
+++ testing/client_scan/client_scan.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/rafaeljusto/shelter/config"
+	"github.com/rafaeljusto/shelter/dao"
+	"github.com/rafaeljusto/shelter/database/mongodb"
+	"github.com/rafaeljusto/shelter/model"
+	"github.com/rafaeljusto/shelter/net/http/rest/protocol"
+	"github.com/rafaeljusto/shelter/testing/utils"
+	"io/ioutil"
+	"labix.org/v2/mgo"
+	"net/http"
+	"time"
+)
+
+var (
+	configFilePath string // Path for the config file with the connection information
+)
+
+func init() {
+	utils.TestName = "ClientScan"
+	flag.StringVar(&configFilePath, "config", "", "Configuration file for client scan test")
+}
+
+func main() {
+	flag.Parse()
+
+	err := utils.ReadConfigFile(configFilePath, &config.ShelterConfig)
+
+	if err == utils.ErrConfigFileUndefined {
+		fmt.Println(err.Error())
+		fmt.Println("Usage:")
+		flag.PrintDefaults()
+		return
+
+	} else if err != nil {
+		utils.Fatalln("Error reading configuration file", err)
+	}
+
+	finishRESTServer := utils.StartRESTServer()
+	defer finishRESTServer()
+	utils.StartWebClient()
+
+	database, databaseSession, err := mongodb.Open(config.ShelterConfig.Database.URI,
+		config.ShelterConfig.Database.Name)
+
+	if err != nil {
+		utils.Fatalln("Error connecting the database", err)
+	}
+	defer databaseSession.Close()
+
+	// If there was some problem in the last test, there could be some data in the
+	// database, so let's clear it to don't affect this test. We avoid checking the error,
+	// because if the collection does not exist yet, it will be created in the first
+	// insert
+	utils.ClearDatabase(database)
+
+	startedAt := createScan(database)
+	retrieveScan(startedAt)
+	retrieveCurrentScan()
+
+	utils.Println("SUCCESS!")
+}
+
+func createScan(database *mgo.Database) time.Time {
+	scanDAO := dao.ScanDAO{
+		Database: database,
+	}
+
+	scan := model.Scan{
+		Status:                   model.ScanStatusExecuted,
+		StartedAt:                time.Now().Add(time.Duration(-2) * time.Minute),
+		FinishedAt:               time.Now().Add(time.Duration(-1) * time.Minute),
+		DomainsScanned:           5000000,
+		DomainsWithDNSSECScanned: 250000,
+		NameserverStatistics: map[string]uint64{
+			"OK":      4800000,
+			"TIMEOUT": 200000,
+		},
+		DSStatistics: map[string]uint64{
+			"OK":     220000,
+			"EXPSIG": 30000,
+		},
+	}
+
+	if err := scanDAO.Save(&scan); err != nil {
+		utils.Fatalln("Error creating scan", err)
+	}
+
+	return scan.StartedAt
+}
+
+func retrieveScan(startedAt time.Time) {
+	var client http.Client
+
+	url := ""
+	if len(config.ShelterConfig.WebClient.Listeners) > 0 {
+		url = fmt.Sprintf("http://%s:%d", config.ShelterConfig.WebClient.Listeners[0].IP,
+			config.ShelterConfig.WebClient.Listeners[0].Port)
+	}
+
+	if len(url) == 0 {
+		utils.Fatalln("There's no interface to connect to", nil)
+	}
+
+	uri := fmt.Sprintf("/scan/%s", startedAt.Format(time.RFC3339Nano))
+	r, err := http.NewRequest("GET", fmt.Sprintf("%s%s", url, uri), nil)
+	if err != nil {
+		utils.Fatalln("Error creating the HTTP request", err)
+	}
+
+	utils.BuildHTTPHeader(r, nil)
+
+	response, err := client.Do(r)
+	if err != nil {
+		utils.Fatalln("Error sending request", err)
+	}
+
+	responseContent, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		utils.Fatalln("Error reading response content", err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		utils.Fatalln(fmt.Sprintf("Expected HTTP status %d and got %d for method GET "+
+			"and URI /scan/currentscan",
+			http.StatusOK, response.StatusCode),
+			errors.New(string(responseContent)),
+		)
+	}
+
+	var scanResponse protocol.ScanResponse
+	if err := json.Unmarshal(responseContent, &scanResponse); err != nil {
+		utils.Fatalln("Error decoding scan response", err)
+	}
+
+	if scanResponse.Status != model.ScanStatusToString(model.ScanStatusExecuted) {
+		utils.Fatalln("Invalid status returned by scan", nil)
+	}
+
+	// Tolerance of one second
+	if scanResponse.StartedAt.Sub(startedAt) > (1*time.Second) ||
+		scanResponse.StartedAt.Sub(startedAt) < (-1*time.Second) {
+		utils.Fatalln("Invalid start date returned by scan", nil)
+	}
+
+	if scanResponse.DomainsScanned != 5000000 {
+		utils.Fatalln("Invalid number of domains scanned returned by scan", nil)
+	}
+
+	if scanResponse.DomainsWithDNSSECScanned != 250000 {
+		utils.Fatalln("Invalid number of domains with DNSSEC scanned returned by scan", nil)
+	}
+
+	if len(scanResponse.NameserverStatistics) != 2 {
+		utils.Fatalln("Invalid nameserver statistics returned by scan", nil)
+	}
+
+	if len(scanResponse.DSStatistics) != 2 {
+		utils.Fatalln("Invalid dsset statistics returned by scan", nil)
+	}
+}
+
+func retrieveCurrentScan() {
+	var client http.Client
+
+	url := ""
+	if len(config.ShelterConfig.WebClient.Listeners) > 0 {
+		url = fmt.Sprintf("http://%s:%d", config.ShelterConfig.WebClient.Listeners[0].IP,
+			config.ShelterConfig.WebClient.Listeners[0].Port)
+	}
+
+	if len(url) == 0 {
+		utils.Fatalln("There's no interface to connect to", nil)
+	}
+
+	r, err := http.NewRequest("GET", fmt.Sprintf("%s%s", url, "/scan/current"), nil)
+	if err != nil {
+		utils.Fatalln("Error creating the HTTP request", err)
+	}
+
+	utils.BuildHTTPHeader(r, nil)
+
+	response, err := client.Do(r)
+	if err != nil {
+		utils.Fatalln("Error sending request", err)
+	}
+
+	responseContent, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		utils.Fatalln("Error reading response content", err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		utils.Fatalln(fmt.Sprintf("Expected HTTP status %d and got %d for method GET "+
+			"and URI /scan/currentscan",
+			http.StatusOK, response.StatusCode),
+			errors.New(string(responseContent)),
+		)
+	}
+
+	var scanResponse protocol.ScanResponse
+	if err := json.Unmarshal(responseContent, &scanResponse); err != nil {
+		utils.Fatalln("Error decoding scan response", err)
+	}
+
+	if scanResponse.Status != model.ScanStatusToString(model.ScanStatusWaitingExecution) {
+		utils.Fatalln("Invalid status returned by current scan", nil)
+	}
+}
---
+++ testing/client_scans/client_scans.go
@@ -0,0 +1,223 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/rafaeljusto/shelter/config"
+	"github.com/rafaeljusto/shelter/dao"
+	"github.com/rafaeljusto/shelter/database/mongodb"
+	"github.com/rafaeljusto/shelter/model"
+	"github.com/rafaeljusto/shelter/net/http/rest/protocol"
+	"github.com/rafaeljusto/shelter/testing/utils"
+	"io/ioutil"
+	"labix.org/v2/mgo"
+	"net/http"
+	"time"
+)
+
+var (
+	configFilePath string // Path for the config file with the connection information
+)
+
+func init() {
+	utils.TestName = "ClientScans"
+	flag.StringVar(&configFilePath, "config", "", "Configuration file for client scans test")
+}
+
+func main() {
+	flag.Parse()
+
+	err := utils.ReadConfigFile(configFilePath, &config.ShelterConfig)
+
+	if err == utils.ErrConfigFileUndefined {
+		fmt.Println(err.Error())
+		fmt.Println("Usage:")
+		flag.PrintDefaults()
+		return
+
+	} else if err != nil {
+		utils.Fatalln("Error reading configuration file", err)
+	}
+
+	finishRESTServer := utils.StartRESTServer()
+	defer finishRESTServer()
+	utils.StartWebClient()
+
+	database, databaseSession, err := mongodb.Open(config.ShelterConfig.Database.URI,
+		config.ShelterConfig.Database.Name)
+
+	if err != nil {
+		utils.Fatalln("Error connecting the database", err)
+	}
+	defer databaseSession.Close()
+
+	// If there was some problem in the last test, there could be some data in the
+	// database, so let's clear it to don't affect this test. We avoid checking the error,
+	// because if the collection does not exist yet, it will be created in the first
+	// insert
+	utils.ClearDatabase(database)
+
+	createScans(database)
+	retrieveScans()
+	retrieveScansWithPagination()
+
+	utils.Println("SUCCESS!")
+}
+
+func createScans(database *mgo.Database) {
+	scanDAO := dao.ScanDAO{
+		Database: database,
+	}
+
+	for i := 0; i < 100; i++ {
+		scan := model.Scan{
+			Status:                   model.ScanStatusExecuted,
+			StartedAt:                time.Now().Add(time.Duration(-i*2) * time.Minute),
+			FinishedAt:               time.Now().Add(time.Duration(-i) * time.Minute),
+			DomainsScanned:           5000000,
+			DomainsWithDNSSECScanned: 250000,
+			NameserverStatistics: map[string]uint64{
+				"OK":      4800000,
+				"TIMEOUT": 200000,
+			},
+			DSStatistics: map[string]uint64{
+				"OK":     220000,
+				"EXPSIG": 30000,
+			},
+		}
+
+		if err := scanDAO.Save(&scan); err != nil {
+			utils.Fatalln("Error creating scan", err)
+		}
+	}
+}
+
+func retrieveScans() {
+	var client http.Client
+
+	url := ""
+	if len(config.ShelterConfig.WebClient.Listeners) > 0 {
+		url = fmt.Sprintf("http://%s:%d", config.ShelterConfig.WebClient.Listeners[0].IP,
+			config.ShelterConfig.WebClient.Listeners[0].Port)
+	}
+
+	if len(url) == 0 {
+		utils.Fatalln("There's no interface to connect to", nil)
+	}
+
+	r, err := http.NewRequest("GET", fmt.Sprintf("%s%s", url, "/scans"), nil)
+	if err != nil {
+		utils.Fatalln("Error creating the HTTP request", err)
+	}
+
+	utils.BuildHTTPHeader(r, nil)
+
+	response, err := client.Do(r)
+	if err != nil {
+		utils.Fatalln("Error sending request", err)
+	}
+
+	responseContent, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		utils.Fatalln("Error reading response content", err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		utils.Fatalln(fmt.Sprintf("Expected HTTP status %d and got %d for method GET "+
+			"and URI /scans",
+			http.StatusOK, response.StatusCode),
+			errors.New(string(responseContent)),
+		)
+	}
+
+	var scansResponse protocol.ScansResponse
+	if err := json.Unmarshal(responseContent, &scansResponse); err != nil {
+		utils.Fatalln("Error decoding scan response", err)
+	}
+
+	if scansResponse.PageSize != 20 {
+		utils.Fatalln("Default page size isn't 20", nil)
+	}
+
+	if scansResponse.Page != 1 {
+		utils.Fatalln("Default page isn't the first one", nil)
+	}
+
+	if scansResponse.NumberOfItems != 100 {
+		utils.Fatalln("Not calculating the correct number of scans", nil)
+	}
+
+	if scansResponse.NumberOfPages != 5 {
+		utils.Fatalln("Not calculating the correct number of pages", nil)
+	}
+
+	if len(scansResponse.Scans) != scansResponse.PageSize {
+		utils.Fatalln("Not returning all desired scans", nil)
+	}
+}
+
+func retrieveScansWithPagination() {
+	var client http.Client
+
+	url := ""
+	if len(config.ShelterConfig.WebClient.Listeners) > 0 {
+		url = fmt.Sprintf("http://%s:%d", config.ShelterConfig.WebClient.Listeners[0].IP,
+			config.ShelterConfig.WebClient.Listeners[0].Port)
+	}
+
+	if len(url) == 0 {
+		utils.Fatalln("There's no interface to connect to", nil)
+	}
+
+	r, err := http.NewRequest("GET", fmt.Sprintf("%s%s", url, "/scans?page=5&pagesize=20"), nil)
+	if err != nil {
+		utils.Fatalln("Error creating the HTTP request", err)
+	}
+
+	utils.BuildHTTPHeader(r, nil)
+
+	response, err := client.Do(r)
+	if err != nil {
+		utils.Fatalln("Error sending request", err)
+	}
+
+	responseContent, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		utils.Fatalln("Error reading response content", err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		utils.Fatalln(fmt.Sprintf("Expected HTTP status %d and got %d for method GET "+
+			"and URI /scans",
+			http.StatusOK, response.StatusCode),
+			errors.New(string(responseContent)),
+		)
+	}
+
+	var scansResponse protocol.ScansResponse
+	if err := json.Unmarshal(responseContent, &scansResponse); err != nil {
+		utils.Fatalln("Error decoding scan response", err)
+	}
+
+	if scansResponse.PageSize != 20 {
+		utils.Fatalln("Default page size isn't 20", nil)
+	}
+
+	if scansResponse.Page != 5 {
+		utils.Fatalln("Default page isn't the first one", nil)
+	}
+
+	if scansResponse.NumberOfItems != 100 {
+		utils.Fatalln("Not calculating the correct number of scans", nil)
+	}
+
+	if scansResponse.NumberOfPages != 5 {
+		utils.Fatalln("Not calculating the correct number of pages", nil)
+	}
+
+	if len(scansResponse.Scans) != scansResponse.PageSize {
+		utils.Fatalln("Not returning all desired scans", nil)
+	}
+}
