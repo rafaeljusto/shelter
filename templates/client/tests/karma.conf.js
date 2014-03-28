7798996b07914e9903e951b998fa61765c703ccb
Rafael Dantas Justo <rafael@registro.br>
2014-03-28 14:35:38 -0300

    Fix comment format

--- templates/client/tests/karma.conf.js
+++ templates/client/tests/karma.conf.js
@@ -1,6 +1,6 @@
-# Copyright 2014 Rafael Dantas Justo. All rights reserved.
-# Use of this source code is governed by a GPL
-# license that can be found in the LICENSE file.
+// Copyright 2014 Rafael Dantas Justo. All rights reserved.
+// Use of this source code is governed by a GPL
+// license that can be found in the LICENSE file.
 
 module.exports = function(config) {
   config.set({
