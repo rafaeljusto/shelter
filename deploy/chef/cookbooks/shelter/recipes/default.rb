fb2e8b89bae879c24d4d0dcc560e19c1ba372cb0
Rafael Justo <rafael@justo.net.br>
2014-04-19 10:14:13 -0300

    TLS isn't default anymore

--- deploy/chef/cookbooks/shelter/recipes/default.rb
+++ deploy/chef/cookbooks/shelter/recipes/default.rb
@@ -2,10 +2,7 @@
 # Cookbook Name:: shelter
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+# Copyright 2014, Rafael Dantas Justo
 
 include_recipe "mongodb::default"
 
@@ -27,6 +24,13 @@ template "/usr/shelter/etc/shelter.conf" do
   owner "root"
   group "root"
   variables(
-    
+    :base_path          => node["shelter"]["base_path"],
+    :log_filename       => node["shelter"]["log_filename"],
+    :db_name            => node["shelter"]["db_name"],
+    :db_uri             => node["shelter"]["db_uri"],
+    :scan_resolver      => node["shelter"]["scan_resolver"],
+    :scan_resolver_port => node["shelter"]["scan_resolver_port"],
+    :smtp_auth_user     => node["shelter"]["smtp_auth_user"],
+    :smtp_auth_pwd      => node["shelter"]["smtp_auth_pwd"]
   )
 end
