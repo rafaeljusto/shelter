295d36b46687ce95e5d40b80b2fdcf1555c0489c
Rafael Dantas Justo <rafael@registro.br>
2014-09-11 09:13:32 -0300

    Begin structuring a better protocol message
    See #100

--- protocol/message.go
+++ protocol/message.go
@@ -10,14 +10,60 @@ import (
 	"net/url"
 )
 
+// List of possible input error types. This is useful for translating input errors to good messages
+// for the end-user in the desired language
+const (
+	ErrorCodeAcceptCharset        ErrorCode = "accept-charset-error"
+	ErrorCodeAccept               ErrorCode = "accept-error"
+	ErrorCodeAcceptLanguage       ErrorCode = "accept-language-error"
+	ErrorCodeAuthorizationMissing ErrorCode = "authorization-missing"
+	ErrorCodeConflict             ErrorCode = "conflict"
+	ErrorCodeContentMD5Missing    ErrorCode = "content-md5-missing"
+	ErrorCodeContentTypeMissing   ErrorCode = "content-type-missing"
+	ErrorCodeDateMissing          ErrorCode = "date-missing"
+	ErrorCodeIfMatchFailed        ErrorCode = "if-match-failed"
+	ErrorCodeIfNoneMatchFailed    ErrorCode = "if-none-match-failed"
+	ErrorCodeInvalidAuthorization ErrorCode = "invalid-authorization"
+	ErrorCodeInvalidContentMD5    ErrorCode = "invalid-content-md5"
+	ErrorCodeInvalidContentType   ErrorCode = "invalid-content-type"
+	ErrorCodeInvalidDateTimeFrame ErrorCode = "invalid-date-time-frame"
+	ErrorCodeInvalidDNSKEY        ErrorCode = "invalid-dnskey"
+	ErrorCodeInvalidDSAlgorithm   ErrorCode = "invalid-ds-algorithm"
+	ErrorCodeInvalidDSDigestType  ErrorCode = "invalid-ds-digest-type"
+	ErrorCodeInvalidFQDN          ErrorCode = "invalid-fqdn"
+	ErrorCodeInvalidHeaderDate    ErrorCode = "invalid-header-date"
+	ErrorCodeInvalidIfMatch       ErrorCode = "invalid-if-match"
+	ErrorCodeInvalidIfNoneMatch   ErrorCode = "invalid-if-none-match"
+	ErrorCodeInvalidIP            ErrorCode = "invalid-ip"
+	ErrorCodeInvalidJSONContent   ErrorCode = "invalid-json-content"
+	ErrorCodeInvalidLanguage      ErrorCode = "invalid-language"
+	ErrorCodeInvalidQueryOrderBy  ErrorCode = "invalid-query-order-by"
+	ErrorCodeInvalidQueryPage     ErrorCode = "invalid-query-page"
+	ErrorCodeInvalidQueryPageSize ErrorCode = "invalid-query-page-size"
+	ErrorCodeInvalidURI           ErrorCode = "invalid-uri"
+	ErrorCodeSectetNotFound       ErrorCode = "secret-not-found"
+)
+
+// ErrorCode will garantee that only known errors are reported
+type ErrorCode string
+
+// Translator represents an object that can be translated to a specific language so we can return it
+// to the client in a specific representation
+type Translator interface {
+	Translate(language *messages.LanguagePack) bool
+}
+
 // MessageResponse struct was created to return a message to the user with more
 // information to easy integrate and solve problems
 type MessageResponse struct {
 	Id      string `json:"id,omitempty"`      // Code for integration systems to automatically solve the problem
+	Field   string `json:"field,omitempty"`   // Field that the message is related to
+	Value   string `json:"value,omitempty"`   // Current value of the field
 	Message string `json:"message,omitempty"` // Message in the user's desired language
 	Links   []Link `json:"links,omitempty"`   // Links associating this message with other resources
 }
 
+// NewMessageResponse builds a message adding necessary links for the response
 func NewMessageResponse(
 	id string, roid string,
 	language *messages.LanguagePack,
@@ -52,4 +98,85 @@ func NewMessageResponse(
 	}
 
 	return messageResponse, nil
+}
+
+// NewMessageResponseWithField builds a message with a specific field adding necessary links for the
+// response. This type of message is better for the end-user determinates what was the problem
+func NewMessageResponseWithField(id string, field, value, roid string,
+	language *messages.LanguagePack) (*MessageResponse, error) {
+
+	messageResponse, err := NewMessageResponse(id, roid, language)
+	if err != nil {
+		return nil, err
+	}
+
+	messageResponse.Field = field
+	messageResponse.Value = value
+	return messageResponse, nil
+}
+
+// Translate fills the convert the message content to the desired language before returning it to
+// the end user. If the message is not found in the desired language it will return false
+func (msg *MessageResponse) Translate(language *messages.LanguagePack) bool {
+	if language != nil && language.Messages != nil {
+		var ok bool
+		msg.Message, ok = language.Messages[string(msg.Id)]
+		return ok
+	}
+
+	// When we don't find the message file, at least add the message id to make it easy to
+	// identify the error. Useful on test scenarios
+	msg.Message = string(msg.Id)
+	return false
+}
+
+// Messages is set of message objects to add methods to make our life easier and to be able to
+// return many messages at once
+type Messages []Translator
+
+// Translate converts every message to the desired end-user language. If one of the messages is not
+// found in the desired language it will return false
+func (m Messages) Translate(language *messages.LanguagePack) bool {
+	translationOK := true
+	for i := range m {
+		if ok := m[i].Translate(language); !ok {
+			translationOK = false
+		}
+	}
+
+	return translationOK
+}
+
+// MessagesHolder make it easy to interact with a single message or a group of messages
+type MessagesHolder struct {
+	msgs Messages
+}
+
+// Add is responsable for inserting new messages or list of messages into an internal buffer
+func (m *MessagesHolder) Add(trs ...Translator) bool {
+	for _, tr := range trs {
+		if tr == nil {
+			return false
+		}
+
+		switch tr.(type) {
+		case *MessageResponse:
+			m.msgs = append(m.msgs, tr.(*MessageResponse))
+		case Messages:
+			m.msgs = append(m.msgs, tr.(Messages)...)
+		default:
+			return false
+		}
+	}
+
+	return true
+}
+
+// Messages returns all the messages that were stored in the MessagesHolder object
+func (m *MessagesHolder) Messages() Translator {
+	if len(m.msgs) == 0 {
+		return nil
+	}
+
+	return m.msgs
 }
--- protocol/message_test.go
+++ protocol/message_test.go
@@ -49,3 +49,374 @@ func TestNewMessageResponse(t *testing.T) {
 		t.Fatal("Accepting an invalid URI as ROID")
 	}
 }
+
+func TestNewMessageResponseWithFields(t *testing.T) {
+	messages.ShelterRESTLanguagePacks = messages.LanguagePacks{
+		Default: "en-US",
+		Packs: []messages.LanguagePack{
+			{
+				GenericName:  "en",
+				SpecificName: "en-US",
+				Messages: map[string]string{
+					"message-id": "test message",
+				},
+			},
+		},
+	}
+	messages.ShelterRESTLanguagePack = &messages.ShelterRESTLanguagePacks.Packs[0]
+
+	messageResponse, err := NewMessageResponseWithField("message-id", "field1", "value1", "/test/1",
+		messages.ShelterRESTLanguagePack)
+	if err != nil {
+		t.Fatal("Should create the message response with fields. Details:", err)
+	}
+
+	if messageResponse.Message != "test message" {
+		t.Error("Did not retrieve the correct message")
+	}
+
+	if messageResponse.Field != "field1" {
+		t.Error("Did not set the correct field")
+	}
+
+	if messageResponse.Value != "value1" {
+		t.Error("Did not set the correct field's value")
+	}
+
+	messageResponse, err = NewMessageResponseWithField("message-id", "field1", "value1", ":",
+		messages.ShelterRESTLanguagePack)
+	if err == nil {
+		t.Fatal("Accepting an invalid URI as ROID")
+	}
+}
+
+func TestMessageResponseTranslate(t *testing.T) {
+	data := []struct {
+		description         string
+		message             MessageResponse
+		language            messages.LanguagePack
+		expectedResult      bool
+		expectedTranslation string
+	}{
+		{
+			description: "Testing translation message when language is not defined",
+			message: MessageResponse{
+				Id: "message-XX",
+			},
+			language: messages.LanguagePack{
+				GenericName:  "en",
+				SpecificName: "en-US",
+				Messages:     nil,
+			},
+			expectedResult:      false,
+			expectedTranslation: "message-XX",
+		},
+		{
+			description: "Testing translation message not found",
+			message: MessageResponse{
+				Id: "message-XX",
+			},
+			language: messages.LanguagePack{
+				GenericName:  "en",
+				SpecificName: "en-US",
+				Messages: map[string]string{
+					"message-id": "test message",
+				},
+			},
+			expectedResult:      false,
+			expectedTranslation: "",
+		},
+		{
+			description: "Testing successful message translation",
+			message: MessageResponse{
+				Id: "message-id",
+			},
+			language: messages.LanguagePack{
+				GenericName:  "en",
+				SpecificName: "en-US",
+				Messages: map[string]string{
+					"message-id": "test message",
+				},
+			},
+			expectedResult:      true,
+			expectedTranslation: "test message",
+		},
+	}
+
+	for i, item := range data {
+		ok := item.message.Translate(&item.language)
+
+		if !ok && item.expectedResult {
+			t.Errorf("Item %d, “%s”: mismatch results. Expecting true", i, item.description)
+
+		} else if ok && !item.expectedResult {
+			t.Errorf("Item %d, “%s”: mismatch results. Expecting false", i, item.description)
+		}
+
+		if item.message.Message != item.expectedTranslation {
+			t.Errorf(
+				"Item %d, “%s”: mismatch results. Expecting '%s'; found '%s'",
+				i,
+				item.description,
+				item.expectedTranslation,
+				item.message.Message,
+			)
+		}
+	}
+}
+
+func TestMessagesTranslate(t *testing.T) {
+	data := []struct {
+		description          string
+		messages             Messages
+		language             messages.LanguagePack
+		expectedResult       bool
+		expectedTranslations []string
+	}{
+		{
+			description: "Testing translations when language is not defined",
+			messages: Messages{
+				&MessageResponse{Id: "message-XX"},
+				&MessageResponse{Id: "message-id1"},
+			},
+			language: messages.LanguagePack{
+				GenericName:  "en",
+				SpecificName: "en-US",
+				Messages:     nil,
+			},
+			expectedResult: false,
+			expectedTranslations: []string{
+				"message-XX",
+				"message-id1",
+			},
+		},
+		{
+			description: "Testing translations for unknown ids",
+			messages: Messages{
+				&MessageResponse{Id: "message-XX"},
+				&MessageResponse{Id: "message-id1"},
+			},
+			language: messages.LanguagePack{
+				GenericName:  "en",
+				SpecificName: "en-US",
+				Messages: map[string]string{
+					"message-id1": "test message 1",
+					"message-id2": "test message 2",
+				},
+			},
+			expectedResult: false,
+			expectedTranslations: []string{
+				"",
+				"test message 1",
+			},
+		},
+		{
+			description: "Testing successful messages translations",
+			messages: Messages{
+				&MessageResponse{Id: "message-id1"},
+				&MessageResponse{Id: "message-id2"},
+			},
+			language: messages.LanguagePack{
+				GenericName:  "en",
+				SpecificName: "en-US",
+				Messages: map[string]string{
+					"message-id1": "test message 1",
+					"message-id2": "test message 2",
+				},
+			},
+			expectedResult: true,
+			expectedTranslations: []string{
+				"test message 1",
+				"test message 2",
+			},
+		},
+	}
+
+	for i, item := range data {
+		ok := item.messages.Translate(&item.language)
+		if !ok && item.expectedResult {
+			t.Errorf("Item %d, “%s”: mismatch results. Expecting true", i, item.description)
+
+		} else if ok && !item.expectedResult {
+			t.Errorf("Item %d, “%s”: mismatch results. Expecting false", i, item.description)
+		}
+
+		messages := []Translator(item.messages)
+		if len(messages) != len(item.expectedTranslations) {
+			t.Fatalf(
+				"Item %d, “%s”: mismatch results. Expecting '%d'; found '%d'",
+				i,
+				item.description,
+				len(item.expectedTranslations),
+				len(messages),
+			)
+		}
+
+		for i, message := range messages {
+			m, ok := message.(*MessageResponse)
+			if !ok {
+				t.Fatalf("Item %d, “%s”: Not a message response!", i, item.description)
+			}
+
+			if m.Message != item.expectedTranslations[i] {
+				t.Errorf(
+					"Item %d, “%s”: mismatch results. Expecting '%s'; found '%s'",
+					i,
+					item.description,
+					item.expectedTranslations[i],
+					m.Message,
+				)
+			}
+		}
+	}
+}
+
+type strangeTranslator struct {
+}
+
+func (s strangeTranslator) Translate(language *messages.LanguagePack) bool {
+	return true
+}
+
+func TestMessageHolderAdd(t *testing.T) {
+	data := []struct {
+		description    string
+		translators    []Translator
+		expectedResult bool
+		expectedNumber int
+	}{
+		{
+			description: "Strange translator object is being add",
+			translators: []Translator{
+				&MessageResponse{Id: "id1"},
+				Messages{
+					&MessageResponse{Id: "id2"},
+					&MessageResponse{Id: "id3"},
+				},
+				strangeTranslator{},
+			},
+			expectedResult: false,
+			expectedNumber: 3,
+		},
+		{
+			description: "Nil translator object is being add",
+			translators: []Translator{
+				&MessageResponse{Id: "id1"},
+				Messages{
+					&MessageResponse{Id: "id2"},
+					&MessageResponse{Id: "id3"},
+				},
+				nil,
+			},
+			expectedResult: false,
+			expectedNumber: 3,
+		},
+		{
+			description: "Normal case with mixed types",
+			translators: []Translator{
+				&MessageResponse{Id: "id1"},
+				Messages{
+					&MessageResponse{Id: "id2"},
+					&MessageResponse{Id: "id3"},
+				},
+			},
+			expectedResult: true,
+			expectedNumber: 3,
+		},
+	}
+
+	for i, item := range data {
+		var messagesHolder MessagesHolder
+		ok := messagesHolder.Add(item.translators...)
+
+		if !ok && item.expectedResult {
+			t.Errorf("Item %d, “%s”: mismatch results. Expecting true", i, item.description)
+
+		} else if ok && !item.expectedResult {
+			t.Errorf("Item %d, “%s”: mismatch results. Expecting false", i, item.description)
+		}
+
+		if len(messagesHolder.msgs) != item.expectedNumber {
+			t.Errorf(
+				"Item %d, “%s”: mismatch results. Expecting '%d'; found '%d'",
+				i,
+				item.description,
+				item.expectedNumber,
+				len(messagesHolder.msgs),
+			)
+		}
+	}
+}
+
+func TestMessageHolderMessages(t *testing.T) {
+	data := []struct {
+		description    string
+		translators    []Translator
+		expectedResult bool
+		expectedNil    bool
+		expectedNumber int
+	}{
+		{
+			description:    "No messages inside",
+			translators:    nil,
+			expectedResult: true,
+			expectedNil:    true,
+			expectedNumber: 0,
+		},
+		{
+			description: "Normal case with mixed types",
+			translators: []Translator{
+				&MessageResponse{Id: "id1"},
+				Messages{
+					&MessageResponse{Id: "id2"},
+					&MessageResponse{Id: "id3"},
+				},
+			},
+			expectedResult: true,
+			expectedNil:    false,
+			expectedNumber: 3,
+		},
+	}
+
+	for i, item := range data {
+		var messagesHolder MessagesHolder
+
+		if item.translators != nil {
+			ok := messagesHolder.Add(item.translators...)
+
+			if !ok && item.expectedResult {
+				t.Errorf("Item %d, “%s”: mismatch results. Expecting true", i, item.description)
+
+			} else if ok && !item.expectedResult {
+				t.Errorf("Item %d, “%s”: mismatch results. Expecting false", i, item.description)
+			}
+		}
+
+		messages := messagesHolder.Messages()
+		if messages != nil && item.expectedNil {
+			t.Fatalf("Item %d, “%s”: mismatch results. Expecting nil object", i, item.description)
+		} else if messages == nil && !item.expectedNil {
+			t.Fatalf("Item %d, “%s”: mismatch results. Unexpected nil object", i, item.description)
+		}
+
+		if item.expectedNil {
+			continue
+		}
+
+		msgs, ok := messages.(Messages)
+		if !ok {
+			t.Fatal("Not a Messages object")
+		}
+		specificMsgs := []Translator(msgs)
+
+		if len(specificMsgs) != item.expectedNumber {
+			t.Errorf(
+				"Item %d, “%s”: mismatch results. Expecting '%d'; found '%d'",
+				i,
+				item.description,
+				item.expectedNumber,
+				len(specificMsgs),
+			)
+		}
+	}
+}
